2e1f164f9b3e76d05f14c75b33036978d43dab6e
Ava Lehner <avalehner@gmail.com>
2026-05-18 13:37:00 -0400

    created months objs

---
+++ src/monthsConfig.ts
@@ -0,0 +1,81 @@
+type Month = {
+  name: string;
+  shortName: string;
+  index: number;
+  color: string;
+};
+
+export const months: Month[] = [
+  {
+    name: 'January',
+    shortName: 'Jan',
+    index: 0,
+    color: '#bef8c2',
+  },
+  {
+    name: 'February',
+    shortName: 'Feb',
+    index: 1,
+    color: '#E8B0D4',
+  },
+  {
+    name: 'March',
+    shortName: 'Mar',
+    index: 2,
+    color: '#52ba59',
+  },
+  {
+    name: 'April',
+    shortName: 'Apr',
+    index: 3,
+    color: '#8878da',
+  },
+  {
+    name: 'May',
+    shortName: 'May',
+    index: 4,
+    color: '#ff7b57',
+  },
+  {
+    name: 'June',
+    shortName: 'Jun',
+    index: 5,
+    color: '#7dba73',
+  },
+  {
+    name: 'July',
+    shortName: 'Jul',
+    index: 6,
+    color: '#208e28',
+  },
+  {
+    name: 'August',
+    shortName: 'Aug',
+    index: 7,
+    color: '#293faa',
+  },
+  {
+    name: 'September',
+    shortName: 'Sep',
+    index: 8,
+    color: '#ae4040',
+  },
+  {
+    name: 'October',
+    shortName: 'Oct',
+    index: 9,
+    color: '#ffdb67',
+  },
+  {
+    name: 'November',
+    shortName: 'Nov',
+    index: 10,
+    color: '#7b4800',
+  },
+  {
+    name: 'December',
+    shortName: 'Dec',
+    index: 11,
+    color: '#000000',
+  },
+];
