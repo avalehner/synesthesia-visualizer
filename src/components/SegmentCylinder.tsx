7b7cdc2a28d0db291e3f8f17691e0e8f10fb0a5c
Ava Lehner <avalehner@gmail.com>
2026-05-19 15:34:03 -0400

    moved mesh + text to segment component in preparation for week/day view

---
+++ src/components/SegmentCylinder.tsx
@@ -0,0 +1,76 @@
+import * as THREE from 'three';
+import type { Segment } from '../types';
+import { Fragment, useState } from 'react';
+import { Text } from '@react-three/drei';
+
+interface SegmentCylinderProps {
+  segments: Segment[];
+  radius: number;
+  height: number;
+  onSegmentClick: (index: number) => void;
+}
+const segmentAngle = (Math.PI * 2) / 12; // 2pi/12 - full circle is 2 pi, need 12 slices for each month
+
+const getSegmentCenterAngle = (index: number) => {
+  const startingOffset = 6 * segmentAngle; //camera points at 180 degrees, want Jan to start there
+  const subtractionAmount = -index * segmentAngle;
+  const centerAngle = startingOffset + subtractionAmount + segmentAngle / 2; //adds 15 degrees to move edge of segment to senter
+  return centerAngle;
+};
+
+const SegmentCylinder = ({ segments, radius, height, onSegmentClick }: SegmentCylinderProps) => {
+  const [hoveredMonth, setHoveredMonth] = useState<number | null>(null);
+
+  return segments.map((segment) => {
+    return (
+      <Fragment key={segment.index}>
+        <mesh
+          position={[0, 0, 0]}
+          onClick={() => onSegmentClick(segment.index)}
+          // () => {
+          //   clickedMonthRef.current = segment.index;
+          //   console.log('clicked month:', segment.label);
+          // }
+          onPointerOver={() => setHoveredMonth(segment.index)}
+          onPointerOut={() => setHoveredMonth(null)}
+        >
+          {' '}
+          {/* positions 5 units in front of the camera */}
+          <cylinderGeometry
+            args={[
+              radius,
+              radius,
+              height,
+              8,
+              1,
+              true,
+              -segment.index * segmentAngle + 6 * segmentAngle,
+              segmentAngle,
+            ]}
+          />
+          {/* cylinder args={[radiusTop, radiusBottom, height, radialSegments, heightSegments, openEnded, thetaStart, thetaLength]} */}
+          <meshStandardMaterial
+            color={segment.color}
+            emissive={hoveredMonth === segment.index ? segment.color : 'black'}
+            emissiveIntensity={hoveredMonth === segment.index ? 0.5 : 0}
+            side={THREE.BackSide}
+          />
+          {/* bright hotpink, easy to see*/}
+        </mesh>
+        <Text
+          position={[
+            //center angle calculation inside sine and cosine
+            Math.sin(getSegmentCenterAngle(segment.index)) * 49.5,
+            0,
+            Math.cos(getSegmentCenterAngle(segment.index)) * 49.5, //reduced radius to 49.5 so its slightly inside wall
+          ]}
+          rotation={[0, Math.PI + getSegmentCenterAngle(segment.index), 0]}
+        >
+          {segment.label}
+        </Text>
+      </Fragment>
+    );
+  });
+};
+
+export default SegmentCylinder;
