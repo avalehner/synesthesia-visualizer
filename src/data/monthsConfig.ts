a61dcc3f3d69e6d427326a2253f289ae2d5a982f
Ava Lehner <avalehner@gmail.com>
2026-05-19 15:33:15 -0400

    moved to data folder

---
+++ src/data/monthsConfig.ts
@@ -0,0 +1,76 @@
+import type { Month } from '../types';
+
+export const months: Month[] = [
+  {
+    name: 'January',
+    shortName: 'Jan',
+    index: 0,
+    color: '#d6ffd9',
+  },
+  {
+    name: 'February',
+    shortName: 'Feb',
+    index: 1,
+    color: '#ffd5f0',
+  },
+  {
+    name: 'March',
+    shortName: 'Mar',
+    index: 2,
+    color: '#81eb88',
+  },
+  {
+    name: 'April',
+    shortName: 'Apr',
+    index: 3,
+    color: '#a494f5',
+  },
+  {
+    name: 'May',
+    shortName: 'May',
+    index: 4,
+    color: '#fba38b',
+  },
+  {
+    name: 'June',
+    shortName: 'Jun',
+    index: 5,
+    color: '#9ddc93',
+  },
+  {
+    name: 'July',
+    shortName: 'Jul',
+    index: 6,
+    color: '#4fd558',
+  },
+  {
+    name: 'August',
+    shortName: 'Aug',
+    index: 7,
+    color: '#4e68ed',
+  },
+  {
+    name: 'September',
+    shortName: 'Sep',
+    index: 8,
+    color: '#f47a7a',
+  },
+  {
+    name: 'October',
+    shortName: 'Oct',
+    index: 9,
+    color: '#ffeaa4',
+  },
+  {
+    name: 'November',
+    shortName: 'Nov',
+    index: 10,
+    color: '#bb8b48',
+  },
+  {
+    name: 'December',
+    shortName: 'Dec',
+    index: 11,
+    color: '#000000',
+  },
+];
