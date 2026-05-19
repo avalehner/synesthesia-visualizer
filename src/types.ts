b679cc36381be09213659e318ca783c315dd069e
Ava Lehner <avalehner@gmail.com>
2026-05-19 15:33:30 -0400

    added segment type

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type Segment = {
+  index: number;
+  label: string;
+  color: string;
+};
+
+export type Month = {
+  name: string;
+  shortName: string;
+  index: number;
+  color: string;
+};
